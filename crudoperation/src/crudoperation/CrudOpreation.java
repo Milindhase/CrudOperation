07e64c7611f1c89d56d7085611f358b7b70d6a9a
Milind Hase <Milind Hase@DESKTOP-4HCB293>
2022-07-03 10:03:16 +0530

    Frist commit...

---
+++ crudoperation/src/crudoperation/CrudOpreation.java
@@ -0,0 +1,22 @@
+package crudoperation;
+
+public class CrudOpreation {
+	  public static void main(String[] args) {
+		  
+		  int a=30,b=20,Add=0,sub=0,mul=0,div=0;
+		  
+		  Add=a+b;
+		  System.out.println("addition===="  + Add);
+		  
+		  sub=a-b;
+		  System.out.println("subtraction===="  + sub);
+		  
+		  mul=a*b;
+		  System.out.println("multiplication===="  + mul);
+		  
+		  div=a/b;
+		  System.out.println("division===="  + div);
+		  
+
+}
+}
